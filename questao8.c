0a728f4ee688ab2625ea0f774c562bff3cc79288
Mateus <mateus.souza.137@ufrn.edu.br>
2024-04-06 21:53:59 -0300

    .

--- questao8.c
+++ questao8.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
+<<<<<<< Updated upstream
   int vet[] = {4, 9, 13};
   int i;
   for(i=0;i<3;i++){
@@ -12,4 +13,18 @@ int main(void) {
   printf("%X ",vet+i);
   }
 
+=======
+    int vet[] = {4, 9, 13};
+    int i;
+    for (i=0;i<3;i++) {
+        printf("%d\n", *(vet+i));
+    }
+    
+
+    for (i=0;i<3;i++) {
+        printf("%X\n", vet+i);
+    }
+    
+    return 0;
+>>>>>>> Stashed changes
 }
