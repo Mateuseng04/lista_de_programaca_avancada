c9cfbdb142eb0601edcbd02ef5a50702b5657bf2
Mateus <mateus.souza.137@ufrn.edu.br>
2024-04-06 22:07:05 -0300

    .

--- questao10.c
+++ questao10.c
@@ -2,16 +2,8 @@
 #include <stdio.h>
 
 int main(void) {
-<<<<<<< Updated upstream
   
   int const *x = 3;
   printf("%d", ++(*x));
   
 }
-=======
-    int const *x;
-    x = 3;
-    printf("%d", *x);
-    return 0;
-}
->>>>>>> Stashed changes
--- questao9.c
+++ questao9.c
@@ -1,6 +1,5 @@
 
 #include <stdio.h>
-<<<<<<< Updated upstream
 
 struct teste{
   int x = 3;
@@ -12,18 +11,4 @@ int main(void){
   printf("%s", s->name);
 
   return 0;
-=======
-#include <stdlib.h>
-
-struct teste{
-    
-    int x = 3;
-    char nome[10]={"Jose"};
-};
-
-int main(void){
-    struct teste *s;
-    printf("%d", s->x);
-    printf("%s", s->nome[10]);
->>>>>>> Stashed changes
 }
